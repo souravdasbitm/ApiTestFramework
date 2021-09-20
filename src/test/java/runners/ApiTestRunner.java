6bedb1fa7b02eeab120cda9d1164224970d7c85d
Sourav Das <44365053+souravdasbitm@users.noreply.github.com>
2021-09-20 16:28:09 +0530

    Add files via upload
    C

---
+++ src/test/java/runners/ApiTestRunner.java
@@ -0,0 +1,17 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/FuntionalApiTest",
+        glue = {"stepDefinitions"},
+        monochrome = true,
+        strict = true
+)
+
+public class ApiTestRunner {
+
+}
